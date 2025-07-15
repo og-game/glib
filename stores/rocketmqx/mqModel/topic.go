65e82b1b9e2f3ba7ede451237c53c9e66bcd390d
ohwin <ohwin@ohwins-Mac-mini.local>
2025-07-15 14:35:21 +0800

    添加topic

--- stores/rocketmqx/mqModel/topic.go
+++ stores/rocketmqx/mqModel/topic.go
@@ -1,5 +1,6 @@
 package mq_model
 
 const (
-	TopicUserGameLog = "user_game_log"
+	TopicUserGameLog             = "user_game_log"
+	TopicUserBalanceChangeRecord = "user_balance_change_record" // 用户账变记录topic
 )
