0725087b4ab52c27b996a9f0a38c33eac3c5b339
leo <leo@gmail.com>
2025-07-17 15:26:40 +0800

    edit rocketmqx

--- stores/rocketmqx/mqModel/model.go
+++ stores/rocketmqx/mqModel/model.go
@@ -2,13 +2,14 @@ package mq_model
 
 type (
 	UserGameLog struct {
-		UserID        int64  `json:"user_id"`
-		GameID        int64  `json:"game_id"`
-		PlatformID    int64  `json:"platform_id"`
-		MerchantID    int64  `json:"merchant_id"`
-		DeviceID      string `json:"device_id"`
-		DeviceIp      string `json:"device_ip"`
-		DeviceOs      string `json:"device_os"`
-		StartPlayTime int64  `json:"start_play_time"`
+		UserID         int64  `json:"user_id"`
+		GameID         int64  `json:"game_id"`
+		PlatformID     int64  `json:"platform_id"`
+		MerchantID     int64  `json:"merchant_id"`
+		MerchantUserID string `json:"merchant_user_id"`
+		DeviceID       string `json:"device_id"`
+		DeviceIp       string `json:"device_ip"`
+		DeviceOs       string `json:"device_os"`
+		StartPlayTime  int64  `json:"start_play_time"`
 	}
 )
