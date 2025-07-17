921cc2c49c0c7f8ed9365bb17f272d001bfb0575
o1was <o1was@gmail.com>
2025-07-17 20:11:01 +0800

    ✨ Feat(topic): topic
    topic

--- stores/rocketmqx/mqModel/topic.go
+++ stores/rocketmqx/mqModel/topic.go
@@ -3,4 +3,5 @@ package mq_model
 const (
 	TopicUserGameLog             = "user_game_log"
 	TopicUserBalanceChangeRecord = "user_balance_change_record" // 用户账变记录topic
+	TopicUserTransferBetRecord   = "user_transfer_bet_record"   // 用户转账游戏记录topic
 )
